268cdcd5459b7345d3ce785c4deccb2665cf6687
Billy Moon <billy.moon@ardoq.com>
2022-10-31 08:53:17 +0000

    remove console.log comment and hopefully trigger ci deploy

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const serverDefaults = {
     "bdf72e1917823ee68d5ce655a5a5b8fce8e17ab555a8b0521bf7322ff83bcdf9",
   ENCRYPTION_IV: "0b6b8035ac5b9449344b1f3ecd313f39",
 };
-// console.log(process)
+
 const serverRuntimeConfig = {
   rootDirname: __dirname,
   ...serverDefaults,
